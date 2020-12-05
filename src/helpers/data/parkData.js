6ab22de4a558bccbaa125721ef4c57facf605a5f
Liz <liz@Liz-s-Mac.local>
2020-12-05 10:29:12 -0600

    add catch error to api calls

--- src/helpers/data/parkData.js
+++ src/helpers/data/parkData.js
@@ -9,21 +9,21 @@ const getParkData = () => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}/parks?limit=5&api_key=${key}`).then((response) => {
     resolve((response.data.data));
     console.warn('parks', (response.data.data));
-  });
+  }).catch((error) => reject(error));
 });
 
 const getCampgroundData = () => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}/campgrounds?limit=5&api_key=${key}`).then((response) => {
     resolve((response.data.data));
     console.warn('camps', (response.data.data));
-  });
+  }).catch((error) => reject(error));
 });
 
 const getHikeData = () => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}/thingstodo?parkCode=${parkCode}&limit=5&api_key=${key}`).then((response) => {
     resolve((response.data.data));
     console.warn('hikes', (response.data.data));
-  });
+  }).catch((error) => reject(error));
 });
 
 export { getParkData, getCampgroundData, getHikeData };
