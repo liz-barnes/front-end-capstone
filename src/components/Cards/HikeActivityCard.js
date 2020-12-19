d07859dfb4d96d423e1629eb1c97769ebf338444
Liz <liz@Liz-s-Mac.local>
2020-12-19 00:44:26 -0600

    finish styling, app completely functional

--- src/components/Cards/HikeActivityCard.js
+++ src/components/Cards/HikeActivityCard.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function HikeCard({ hike, removeActivity, onUpdate }) {
   return (
-    <div>
+    <div className="hike-activity-card-container">
       <Link to={{
         pathname: '/parks/hikess/',
         state: {
@@ -12,8 +12,8 @@ export default function HikeCard({ hike, removeActivity, onUpdate }) {
       }}>
         <img className="hike-image" id={hike.id} src={hike.images[0].url} alt={hike.images[0].altText}/>
       </Link>
-      <h3>{hike.title}</h3>
       <button className="delete-activity-btn" onClick={() => removeActivity(hike.id)} >Delete from Trip</button>
+      <h3>{hike.title}</h3>
     </div>
   );
 }
