9b6f4323d9c92586bd17c6d0d3a34e5827744a86
Liz <liz@Liz-s-Mac.attlocal.net>
2021-09-05 21:00:18 -0500

    state abbreviation switch case

--- src/components/ParkSearch/index.js
+++ src/components/ParkSearch/index.js
@@ -18,6 +18,167 @@ export default function ParkSearch() {
     }
   }, [allParks]);
 
+  const stateAbbreviation = (state) => {
+    switch (state) {
+      case 'ALABAMA':
+        return 'AL';
+
+      case 'ALASKA':
+        return 'AK';
+
+      case 'ARIZONA':
+        return 'AZ';
+
+      case 'ARKANSAS':
+        return 'AR';
+
+      case 'CALIFORNIA':
+        return 'CA';
+
+      case 'COLORADO':
+        return 'CO';
+
+      case 'CONNECTICUT':
+        return 'CT';
+
+      case 'DELAWARE':
+        return 'DE';
+
+      case 'DISTRICT OF COLUMBIA':
+        return 'DC';
+
+      case 'FLORIDA':
+        return 'FL';
+
+      case 'GEORGIA':
+        return 'GA';
+
+      case 'HAWAII':
+        return 'HI';
+
+      case 'IDAHO':
+        return 'ID';
+
+      case 'ILLINOIS':
+        return 'IL';
+
+      case 'INDIANA':
+        return 'IN';
+
+      case 'IOWA':
+        return 'IA';
+
+      case 'KANSAS':
+        return 'KS';
+
+      case 'KENTUCKY':
+        return 'KY';
+
+      case 'LOUISIANA':
+        return 'LA';
+
+      case 'MAINE':
+        return 'ME';
+
+      case 'MARYLAND':
+        return 'MD';
+
+      case 'MASSACHUSETTS':
+        return 'MA';
+
+      case 'MICHIGAN':
+        return 'MI';
+
+      case 'MINNESOTA':
+        return 'MN';
+
+      case 'MISSISSIPPI':
+        return 'MS';
+
+      case 'MISSOURI':
+        return 'MO';
+
+      case 'MONTANA':
+        return 'MT';
+
+      case 'NEBRASKA':
+        return 'NE';
+
+      case 'NEVADA':
+        return 'NV';
+
+      case 'NEW HAMPSHIRE':
+        return 'NH';
+
+      case 'NEW JERSEY':
+        return 'NJ';
+
+      case 'NEW MEXICO':
+        return 'NM';
+
+      case 'NEW YORK':
+        return 'NY';
+
+      case 'NORTH CAROLINA':
+        return 'NC';
+
+      case 'NORTH DAKOTA':
+        return 'ND';
+
+      case 'OHIO':
+        return 'OH';
+
+      case 'OKLAHOMA':
+        return 'OK';
+
+      case 'OREGON':
+        return 'OR';
+
+      case 'PENNSYLVANIA':
+        return 'PA';
+
+      case 'RHODE ISLAND':
+        return 'RI';
+
+      case 'SOUTH CAROLINA':
+        return 'SC';
+
+      case 'SOUTH DAKOTA':
+        return 'SD';
+
+      case 'TENNESSEE':
+        return 'TN';
+
+      case 'TEXAS':
+        return 'TX';
+
+      case 'UTAH':
+        return 'UT';
+
+      case 'VERMONT':
+        return 'VT';
+
+      case 'VIRGINIA':
+        return 'VA';
+
+      case 'WASHINGTON':
+        return 'WA';
+
+      case 'WEST VIRGINIA':
+        return 'WV';
+
+      case 'WISCONSIN':
+        return 'WI';
+
+      case 'WYOMING':
+        return 'WY';
+      default:
+        return 'State Not Found';
+    }
+  };
+
+  const showResults = () => searchResults.map((park) => <AdventureCard key={park.id} park={park} />);
+
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     setSearchResults(allParks?.filter((park) => park.name.toLowerCase().includes(searchInput.toLowerCase())));
@@ -28,17 +189,27 @@ export default function ParkSearch() {
   return (
     <div className="park-search-page">
       <div className="page-banner">
-        <h1 className="banner-heading" >National Parks</h1>
+        <h1 className="banner-heading">National Parks</h1>
       </div>
       <div className="search-bar">
-        <form className="form-inline my-2 my-lg-0" onSubmit={handleSearchSubmit} >
-          <input className="search-input form-control" name="searchInput" value={searchInput.name} type="text" placeholder="Search park by name"
-          aria-label="Search" onChange={(e) => setSearchInput(e.target.value)}/>
+        <form
+          className="form-inline my-2 my-lg-0"
+          onSubmit={handleSearchSubmit}
+        >
+          <input
+            className="search-input form-control"
+            name="searchInput"
+            value={searchInput.name}
+            type="text"
+            placeholder="Search park by name"
+            aria-label="Search"
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
         </form>
         <div className="park-search-results-container">
-        {/* {suggestParks && suggestedParks !== null ? suggestedParks.map((park) => <AdventureCard key={park.id} park={park} />) : ''} */}
-          {searchSubmit && searchResults.length ? searchResults.map((park) => <AdventureCard key={park.id} park={park} />) : '' }
-          {searchSubmit && !searchResults.length ? <h1>No Parks Found</h1> : '' }
+          {/* {suggestParks && suggestedParks !== null ? suggestedParks.map((park) => <AdventureCard key={park.id} park={park} />) : ''} */}
+          {searchSubmit && searchResults.length ? showResults() : ''}
+          {searchSubmit && !searchResults.length ? <h1>No Parks Found</h1> : ''}
         </div>
       </div>
     </div>
