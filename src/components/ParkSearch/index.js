7b1a75daf81aead73ffcc122a52ec1277fe67306
Liz <liz@Liz-s-Mac.attlocal.net>
2021-09-05 19:03:13 -0500

    searching for park and printing card to DOM

--- src/components/ParkSearch/index.js
+++ src/components/ParkSearch/index.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
-// import AdventureCard from '../Cards/AdventureCard';
-// import Search from '../Search';
+import AdventureCard from '../Cards/AdventureCard';
 import parkData from '../../helpers/data/parkData';
-// import park from '../../helpers/data/parkObject';
 
 export default function ParkSearch() {
   const [allParks, setAllParks] = useState([]);
@@ -11,36 +9,11 @@ export default function ParkSearch() {
   const [searchResults, setSearchResults] = useState([], []);
   const [searchInput, setSearchInput] = useState('');
   const [searchSubmit, setSearchSubmit] = useState(null);
-  const test = [
-    {
-      name: 'bubbles',
-      age: 3,
-    },
-    {
-      name: 'cartoon',
-    },
-    {
-      name: 'nip',
-    },
-    {
-      name: 'tip',
-    },
-  ];
-
-  // const getAllParks = () => {
-  //   parkData.getParkData().then((response) => {
-  //     console.warn('parkz', response);
-  //     setAllParks(response);
-  //     console.warn(allParks, 'you did it');
-  //   });
-  // };
 
   useEffect(() => {
     if (!allParks.length) {
       parkData.getParkData().then((response) => {
-        console.warn('parkz', response);
         setAllParks(response);
-        console.warn(allParks, 'you did it');
       });
     }
   });
@@ -50,7 +23,6 @@ export default function ParkSearch() {
     setSearchResults(allParks?.filter((park) => park.name.toLowerCase().includes(searchInput.toLowerCase())));
     setSearchSubmit(true);
     setShowSuggestedParks(false);
-    // this.showParkResults();
   };
 
   return (
@@ -63,38 +35,12 @@ export default function ParkSearch() {
           <input className="search-input form-control" name="searchInput" value={searchInput.name} type="text" placeholder="Search park by name"
           aria-label="Search" onChange={(e) => setSearchInput(e.target.value)}/>
         </form>
-        {searchResults.map((r) => <h1>{r.name}</h1>)}
+        <div className="park-search-results-container">
+        {/* {suggestParks && suggestedParks !== null ? suggestedParks.map((park) => <AdventureCard key={park.id} park={park} />) : ''} */}
+          {searchSubmit && searchResults.length ? searchResults.map((park) => <AdventureCard key={park.id} park={park} />) : '' }
+          {searchSubmit && !searchResults.length ? <h1>No Parks Found</h1> : '' }
+        </div>
       </div>
     </div>
   );
 }
-
-//   showParkResults = () => {
-//     const { searchSubmit, searchResult } = this.state;
-//     if (searchSubmit === true && searchResult.length) {
-//       searchResult.map((park) => <AdventureCard key={park.id} park={park} />);
-//     } else {
-//       <h1>No Parks Found</h1>;
-//     }
-//   }
-
-//   render() {
-//     const {
-//       searchResult, searchSubmit, suggestParks,
-//     } = this.state;
-//     const { suggestedParks } = this.props;
-//     return (
-//       <div className="park-search-page">
-//         <div className="page-banner">
-//           <h1 className="banner-heading" >National Parks</h1>
-//         </div>
-//         <Search handleSearchInput={(e) => this.handleSearchInput(e)} handleSearchSubmit={(e) => this.handleSearchSubmit(e)} value={this.state.searchInput}/>
-//         <div className="park-search-results-container">
-//           {suggestParks && suggestedParks !== null ? suggestedParks.map((park) => <AdventureCard key={park.id} park={park} />) : ''}
-//           {searchSubmit && searchResult.length ? searchResult.map((park) => <AdventureCard key={park.id} park={park} />) : '' }
-//           {searchSubmit && searchResult.length === 0 ? <h1>No Parks Found</h1> : ''}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
