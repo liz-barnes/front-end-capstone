13c38edb6360e905d082cf8e5316ccb578a05f44
Liz <liz@Liz-s-Mac.attlocal.net>
2021-09-19 16:41:24 -0500

    style park url

--- src/views/SingleParkView.js
+++ src/views/SingleParkView.js
@@ -21,14 +21,16 @@ export default function SingleView({
   return (
     <div className="park-page">
         <div className="header-details">
-        <a
-            href={park.url}
-            target="_blank"
-            rel="noreferrer"
-            className="view-code-link"
-            >
-            <h4>{park.name}</h4>
+          {park.url ? (
+            <a
+              href={park.url}
+              target="_blank"
+              rel="noreferrer"
+              className="view-code-link"
+              >
+              <h4>{park.name}</h4>
             </a>
+          ) : <h4>{park.name}</h4> }
           {/* <h6>{park.states}</h6> */}
         </div>
         <div className="header-hikes-btn">
