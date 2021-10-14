d474005d306e03b3ead95edef14a30a8ebd63283
Liz <liz@Liz-s-Mac.local>
2021-10-14 16:41:43 -0500

    revert ParkHikes component to class function

--- src/views/ParkHikesView.js
+++ src/views/ParkHikesView.js
@@ -1,82 +1,64 @@
-import React, { Component, useEffect, useState } from 'react';
-import hikeData from '../helpers/data/hikeData';
-// import { getParkHike } from '../helpers/data/hikeData';
+import React, { Component } from 'react';
+import { getParkHike } from '../helpers/data/hikeData';
 import HikeCard from '../Components/Cards/HikeCard';
 import Loader from '../Components/Loader';
-import { HIKES_PER_PAGE } from '../helpers/constants';
 
-export default function ParkHikes({ ...props }) {
-  const [loading, setLoading] = useState(true);
-  const [hikes, setHikes] = useState([]);
-  const { parkCode } = 'acad';
-  // state = {
-  //   loading: true,
-  //   hike: [],
-  // };
+export default class ParkHikes extends Component {
+  state = {
+    loading: true,
+    hike: [],
+  };
 
-  // const [page, setPage] = useState(1);
-  // const startIndex = (page - 1) * HIKES_PER_PAGE;
-  // const selectedData = data.slice(startIndex, startIndex + HIKES_PER_PAGE);
-
-  useEffect(() => {
-    if (loading && !hikes.length) {
-      console.warn('props', props);
-      hikeData.getParkHike({ parkCode }).then((response) => {
-        console.warn('hike resp', response);
-        setHikes(response);
-        setLoading(false);
-      });
-    }
-  }, [hikes, loading, parkCode, setLoading]);
-
-  // componentDidMount() {
-  //   this.getParkHikes();
-  // }
+  componentDidMount() {
+    this.getParkHikes();
+  }
 
   // think about mapping over each hike to find activity with name "Hiking"
-  // getParkHikes = () => {
-  //   const { singlePark } = this.props.location.state;
-  //   getParkHike(singlePark.parkCode).then((hike) => {
-  //     this.setState({ parkHikes: hike });
-  //   }).then(() => {
-  //     this.setLoading();
-  //   });
-  // }
+  getParkHikes = () => {
+    const { singlePark } = this.props.location.state;
+    getParkHike(singlePark.parkCode).then((hike) => {
+      this.setState({ parkHikes: hike });
+    }).then(() => {
+      this.setLoading();
+    });
+  }
 
   // set loading to false after 100 to stop loader and print data to DOM
-  // setLoading = () => {
-  //   this.timer = setInterval(() => {
-  //     this.setState({ loading: false });
-  //   }, 100);
-  // }
+  setLoading = () => {
+    this.timer = setInterval(() => {
+      this.setState({ loading: false });
+    }, 100);
+  }
 
-  // componentWillUnmount() {
-  //   clearInterval(this.timer);
-  // }
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
 
-  // showHikes = () => {
-  //   const { parkHikes } = this.state;
-  //   if (parkHikes) {
-  //     parkHikes.map((hike) => <HikeCard key={hike.id} hike={hike} />);
-  //   } else {
-  //     <h3>No Hikes Found</h3>;
-  //   }
-  // }
-  // const { singlePark } = this.props.location.state;
-  // const { loading, parkHikes } = this.state;
+  showHikes = () => {
+    const { parkHikes } = this.state;
+    if (parkHikes) {
+      parkHikes.map((hike) => <HikeCard key={hike.id} hike={hike} />);
+    } else {
+      <h3>No Hikes Found</h3>;
+    }
+  }
 
-  return (
-    <>
-      <h1 className='park-hikes-header'>Hikes</h1>
-      <div className="hike-result-container">
-        { loading ? (
-          <Loader />
-        ) : (
-          <p>Hikes go here</p>
-          // hikes.map((hike) => console.warn('hikeypoo', hike))
-        )}
-        {/* {!loading && parkHikes.length === 0 ? <h3>No Hikes Found</h3> : ''} */}
-      </div>
-    </>
-  );
+  render() {
+    const { singlePark } = this.props.location.state;
+    const { loading, parkHikes } = this.state;
+
+    return (
+      <>
+        <h1 className='park-hikes-header'>{singlePark.name} Hikes</h1>
+        <div className="hike-result-container">
+          { loading ? (
+            <Loader />
+          ) : (
+            parkHikes.map((hike) => <HikeCard key={hike.id} hike={hike} />)
+          )}
+          {!loading && parkHikes.length === 0 ? <h3>No Hikes Found</h3> : ''}
+        </div>
+      </>
+    );
+  }
 }
