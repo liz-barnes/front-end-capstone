a1606beabdb76f29350c431364d50c98a03bc958
Liz Barnes <embarnes95@gmail.com>
2021-11-02 14:07:26 -0500

    attempted to updated App to functional component but ran into multiple re-renders on user state change

--- src/App/App.js
+++ src/App/App.js
@@ -1,85 +1,95 @@
-/* eslint-disable import/no-duplicates */
+/* eslint-disable import/no-unresolved */
+// /* eslint-disable import/no-duplicates */
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import fbConnection from '../helpers/data/connection';
 import './App.scss';
-import hikeData from '../helpers/data/parkData';
 import Routes from '../helpers/routes';
-import MyNavbar from '../Components/MyNavbar';
-import { getUserTrips } from '../helpers/data/tripData';
+import MyNavbar from '../Components/MyNavbar/index';
+// import { getUserTrips } from '../helpers/data/tripData';
 
 fbConnection();
 class App extends React.Component {
   state = {
     user: null,
-    userTrips: [],
-    parks: [],
-    hikes: [],
-    campgrounds: [],
-    suggestedParks: null,
+    // userTrips: [],
+    // parks: [],
+    // hikes: [],
+    // campgrounds: [],
+    // suggestedParks: null,
   }
 
   componentDidMount() {
     this.removeListener = firebase.auth().onAuthStateChanged((user) => {
       if (user) {
+        // console.warn('user resp', user);
         this.setState({ user });
       } else {
         this.setState({ user: false });
+        // console.warn('user false', user);
       }
     });
-    console.warn('app running');
-    this.getHikes();
-    // this.getCampgrounds();
+    // console.warn('app running');
   }
 
   componentWillUnmount() {
     this.removeListener();
   }
 
-  getHikes = () => {
-    hikeData.getHikeData().then((resp) => {
-      this.setState({
-        hikes: resp,
-      });
-    });
-  }
+  // getParks = () => {
+  //   getParkData().then((resp) => {
+  //     this.setState({
+  //       parks: resp,
+  //     });
+  //   }).then((response) => {
+  //     this.setSuggestedParks();
+  //   });
+  // }
+
+  // getHikes = () => {
+  //   getHikeData().then((resp) => {
+  //     this.setState({
+  //       hikes: resp,
+  //     });
+  //   });
+  // }
 
   // getCampgrounds = () => {
-  //   campgroundData.getCampgroundData().then((resp) => {
+  //   getCampgroundData().then((resp) => {
   //     this.setState({
   //       campgrounds: resp,
   //     });
   //   });
   // }
 
-  getUserTrips = () => {
-    const { user } = this.state;
-    if (user) {
-      getUserTrips(user.uid).then((resp) => {
-        this.setState({
-          userTrips: resp,
-        });
-      });
-    }
-  }
+  // getUserTrips = () => {
+  //   const { user } = this.state;
+  //   if (user) {
+  //     getUserTrips(user.uid).then((resp) => {
+  //       this.setState({
+  //         userTrips: resp,
+  //       });
+  //     });
+  //   }
+  // }
 
-  setSuggestedParks = () => {
-    const { parks } = this.state;
-    const suggested = [parks[241], parks[467], parks[34]];
-    this.setState({ suggestedParks: suggested });
-  }
+  // setSuggestedParks = () => {
+  //   const { parks } = this.state;
+  //   const suggested = [parks[241], parks[467], parks[34]];
+  //   this.setState({ suggestedParks: suggested });
+  // }
 
   render() {
     const {
-      user, parks, userTrips, suggestedParks,
+      user,
     } = this.state;
     return (
       <div className="App">
         <Router>
           <MyNavbar user={user} />
-          { user !== null && <Routes user={user} parks={parks} userTrips={userTrips} suggestedParks={suggestedParks}/> }
+          { user !== null && <Routes user={user}/> }
         </Router>
      </div>
     );
@@ -87,3 +97,77 @@ class App extends React.Component {
 }
 
 export default App;
+
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
+// import React, { useEffect, useState } from 'react';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import fbConnection from '../helpers/data/connection';
+// import './App.scss';
+// import Routes from '../helpers/routes';
+// import MyNavbar from '../Components/MyNavbar';
+
+// fbConnection();
+// export default function App() {
+//   const [user, setUser] = useState(null);
+//   // const [userExist, setUserExist] = useState(false);
+
+//   useEffect(() => {
+//     if (!user) {
+//       firebase.auth().onAuthStateChanged((userResponse) => {
+//         if (userResponse) {
+//           console.warn('user resp', userResponse);
+//           setUser(userResponse);
+//           // setUserExist(true);
+//           console.warn('user on mount', user);
+//         } else {
+//           setUser(false);
+//           // setUserExist(false);
+//           console.warn('user false', user);
+//         }
+//       });
+//       console.warn('app running');
+//       console.warn('user end on function', user);
+//     } else {
+//       console.warn('user is set', user);
+//     }
+//     console.warn('app running');
+//   }, [user]);
+//   // if (!user) {
+//   //   firebase.auth().onAuthStateChanged((userResponse) => {
+//   //     if (userResponse) {
+//   //       console.warn('user resp', userResponse);
+//   //       setUser(userResponse);
+//   //       console.warn('user on mount', user);
+//   //     } else {
+//   //       setUser(false);
+//   //       console.warn('user false', user);
+//   //     }
+//   //   });
+
+//   // getHikes = () => {
+//   //   hikeData.getHikeData().then((resp) => {
+//   //     this.setState({
+//   //       hikes: resp,
+//   //     });
+//   //   });
+//   // }
+
+//   // getCampgrounds = () => {
+//   //   campgroundData.getCampgroundData().then((resp) => {
+//   //     this.setState({
+//   //       campgrounds: resp,
+//   //     });
+//   //   });
+//   // }
+
+//   return (
+//     <div className="App">
+//       <Router>
+//         <MyNavbar user={user} />
+//         { user !== null && <Routes user={user}/> }
+//         {console.warn('user in render', user)}
+//       </Router>
+//     </div>
+//   );
+// }
